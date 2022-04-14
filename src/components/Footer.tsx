4d41f505c95b42334415cd42e9197753b0471390
Carlos Solano <cs28552758@gmail.com>
2022-04-14 00:15:37 -0500

    font size icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ const Footer = () => {
     <div className="bg-[#652682] h-[118px] flex justify-center items-center">
       <div>
         <i className="fa-brands fa-discord text-white text-3xl"></i>
-        <i className="fa-brands fa-twitter text-white"></i>
-        <i className="fa-brands fa-facebook-f text-white"></i>
-        <i className="fa-brands fa-youtube text-white"></i>
-        <i className="fa-brands fa-instagram text-white"></i>
-        <i className="fa-brands fa-tiktok text-white"></i>
+        <i className="fa-brands fa-twitter text-white  text-3xl"></i>
+        <i className="fa-brands fa-facebook-f text-white  text-3xl"></i>
+        <i className="fa-brands fa-youtube text-white  text-3xl"></i>
+        <i className="fa-brands fa-instagram text-white  text-3xl"></i>
+        <i className="fa-brands fa-tiktok text-white  text-3xl"></i>
       </div>
     </div>
   );
