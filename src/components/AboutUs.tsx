27b32dbae9e27400694cfb1f017fbaca7538a1f7
Omar Campos <omarjc-1997@hotmail.com>
2022-04-14 00:14:21 -0500

    fix font size

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -5,11 +5,11 @@ const AboutUs = () => {
     <div className="text-white mb-20">
       <div className="py-28  grid grid-cols-2">
         <div className="mx-24 ">
-          <h2 className="text-[#8108B9] text-7xl mb-10">
+          <h2 className="text-[#8108B9] text-6xl mb-10">
             Quiénes <span className="text-white"> somos</span>
           </h2>
 
-          <p className="text-[27px]">
+          <p className="text-3xl">
             Somos un equipo multidisciplinario enfocado en brindar soluciones innovadoras basadas en la aplicación de
             tecnologías blockchain, con ello buscamos innovar continuamente en el mundo financiero global fundamentado
             en parámetros de eficiencia e integridad. Huit’s Investment nace como un holding cuya premisa fundamental es
@@ -25,12 +25,12 @@ const AboutUs = () => {
         <Infinite />
 
         <div className="mx-24 flex flex-col items-end justify-end -m-32">
-          <h2 className="text-[#8108B9] text-7xl mb-5 self-end">Nuestro propósito</h2>
+          <h2 className="text-[#8108B9] text-6xl mb-5 self-end">Nuestro propósito</h2>
 
           <span className="text-white text-5xl"> y nuestros valores</span>
         </div>
       </div>
-      <p className="text-[27px] mx-24">
+      <p className="text-3xl mx-24">
         En Huit's investment nuestro propósito es democratizar el acceso al mercado bursátil y financiero teniendo como
         fundamento una adecuada administración del riesgo de mercado, así como la búsqueda continua de oportunidades en
         un mundo competitivo, que beneficien los intereses de nuestros stakeholders promoviendo así un crecimiento tanto
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
     <div className="bg-[#652682] h-[118px] flex justify-center items-center">
-      <div>
-        <i className="fa-brands fa-discord text-white text-3xl"></i>
-        <i className="fa-brands fa-twitter text-white"></i>
-        <i className="fa-brands fa-facebook-f text-white"></i>
-        <i className="fa-brands fa-youtube text-white"></i>
-        <i className="fa-brands fa-instagram text-white"></i>
-        <i className="fa-brands fa-tiktok text-white"></i>
+      <div className="flex flex-row justify-center items-center">
+        <i className="fa-brands fa-discord text-white text-3xl" />
+        <i className="fa-brands fa-twitter text-white" />
+        <i className="fa-brands fa-facebook-f text-white" />
+        <i className="fa-brands fa-youtube text-white" />
+        <i className="fa-brands fa-instagram text-white" />
+        <i className="fa-brands fa-tiktok text-white" />
       </div>
     </div>
   );
