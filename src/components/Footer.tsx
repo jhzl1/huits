9781b05f820bf7f39e11f0736f956a988ddcd907
Omar Campos <omarjc-1997@hotmail.com>
2022-04-14 00:10:06 -0500

    center icons in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
 const Footer = () => {
   return (
-    <div className="bg-[#652682] h-[118px]">
-      <i className="fa-brands fa-discord text-white absolute w-22px h-15px"></i>
-      <i className="fa-brands fa-twitter text-white"></i>
-      <i className="fa-brands fa-facebook-f text-white"></i>
-      <i className="fa-brands fa-youtube text-white"></i>
-      <i className="fa-brands fa-instagram text-white"></i>
-      <i className="fa-brands fa-tiktok text-white"></i>
+    <div className="bg-[#652682] h-[118px] flex justify-center items-center">
+      <div>
+        <i className="fa-brands fa-discord text-white text-3xl"></i>
+        <i className="fa-brands fa-twitter text-white"></i>
+        <i className="fa-brands fa-facebook-f text-white"></i>
+        <i className="fa-brands fa-youtube text-white"></i>
+        <i className="fa-brands fa-instagram text-white"></i>
+        <i className="fa-brands fa-tiktok text-white"></i>
+      </div>
     </div>
   );
 };
